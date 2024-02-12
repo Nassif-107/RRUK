18f9e651fffbf569c6cae410d60ae2f54336d053
Nassif107 <basmanassif9@gmail.com>
2024-02-12 10:24:47 +0300

    start testing

---
+++ Labs/Лаба_01/regressionTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using РРУК_01;
+
+namespace regressionTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
