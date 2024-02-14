d9ed032031391576cd1630b8369d78b997b1d63a
Nassif107 <basmanassif9@gmail.com>
2024-02-14 09:57:13 +0300

    start third lab

---
+++ Labs/Лаба_03/РРУК_01/Bill.cs
@@ -0,0 +1,97 @@
+﻿namespace РРУК_01
+{
+    public class Bill
+    {
+        private List<Item> _items;
+        private Customer _customer;
+        public Bill(Customer customer)
+        {
+            this._customer = customer;
+            this._items = new List<Item>();
+        }
+        public void addGoods(Item arg)
+        {
+            _items.Add(arg);
+        }
+        // Метод для вывода оглавления
+        public string GetHeader()
+        {
+            return "Счет для " + _customer.getName() + "\n" +
+                "\t" + "Название" + "\t" + "Цена" +
+                "\t" + "Кол-во" + "Стоимость" + "\t" + "Скидка" +
+                "\t" + "Сумма" + "\t" + "Бонус" + "\n";
+        }
+        // Метод для вывода результата
+        public string GetFooter(double totalAmount, int totalBonus)
+        {
+            return "Сумма счета составляет " + 
+                totalAmount.ToString() + "\n" + 
+                "Вы заработали " + totalBonus.ToString() + 
+                " бонусных балов";
+        }
+        // Метод для вывода списка товаров
+        public string GetItemString(Item each, double discount,
+            double totalAmount, int bonus)
+        {
+            return "\t" + each.getGoods().getTitle() + "\t" +
+                "\t" + each.getPrice() + "\t" + each.getQuantity() +
+                "\t" + (GetSum(each)).ToString() +
+                "\t" + discount.ToString() + "\t" + totalAmount.ToString() +
+                "\t" + bonus.ToString() + "\n";
+        }
+        
+        
+        // Метод для вычисления суммы
+        public double GetSum(Item each)
+        {
+            return each.getQuantity() * each.getPrice();
+        }
+        //---Метод получения использованных бонусов
+        public double GetUsedBonus(Item each, double sumWithDiscount)
+        {
+            double usedBonus = 0;
+            switch (each.getGoods().getPriceCode())
+            {
+                //Обычный товав
+                case Goods.REGULAR:
+                    if (each.getQuantity() > 5)
+                        usedBonus = _customer.useBonus((int)(sumWithDiscount));
+                    break;
+                //Специальное предложение
+                case Goods.SPECIAL_OFFER:
+                    if (each.getQuantity() > 1)
+                        usedBonus = _customer.useBonus((int)(sumWithDiscount));
+                    break;
+            }
+            return usedBonus;
+        }
+        public String statement()
+        {
+            double totalAmount = 0;
+            int totalBonus = 0;
+            List<Item>.Enumerator items = _items.GetEnumerator();
+            String result = GetHeader();
+            while (items.MoveNext())
+            {
+                Item each = (Item)items.Current;
+                
+                //определить сумму для каждой строки
+                double discount = each.GetDiscount();
+                int bonus = each.GetBonus();
+
+                //показать результаты
+                double sumWithDiscount = GetSum(each) - discount;
+                double usedBonus = GetUsedBonus(each, sumWithDiscount);
+                double thisAmount = sumWithDiscount - usedBonus;
+                result += GetItemString(each, discount, thisAmount, bonus);
+                totalAmount += thisAmount;
+                totalBonus += bonus;
+            }
+            //добавить нижний колонтитул
+            result += GetFooter(totalAmount, totalBonus);
+            //Запомнить бонус клиента
+            _customer.receiveBonus(totalBonus);
+            return result;
+        }
+    }
+}
---
+++ Labs/Лаба_03/РРУК_01/Goods.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РРУК_01
+{
+    public class Goods
+    {
+        public const int REGULAR = 0;
+        public const int SALE = 1;
+        public const int SPECIAL_OFFER = 2;
+        private String _title;
+        private int _priceCode;
+        public Goods(String title, int priceCode)
+        {
+            _title = title;
+            _priceCode = priceCode;
+        }
+        public int getPriceCode()
+        {
+            return _priceCode;
+        }
+        public void setPriceCode(int arg)
+        {
+            _priceCode = arg;
+        }
+        public String getTitle()
+        {
+            return _title;
+        }
+        // Метод для получения бонуса
+        public virtual int GetBonus(int _quantity,double _price) 
+        {
+
+        }
+        // Метод для получения скидки
+        public double GetDiscount(int _quantity, double _price)
+        {
+            double discount = 0;
+            switch (_priceCode)
+            {
+                case Goods.REGULAR:
+                    if (_quantity > 2)
+                        discount = (_quantity * _price) * 0.03; // 3%
+                    break;
+                case Goods.SPECIAL_OFFER:
+                    if (_quantity > 10)
+                        discount = (_quantity * _price) * 0.005; // 0.5%
+                    break;
+                case Goods.SALE:
+                    if (_quantity > 3)
+                        discount = (_quantity * _price) * 0.01; // 0.1%
+                    break;
+            }
+            return discount;
+        }
+    }
+}
